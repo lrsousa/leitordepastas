c8600d7618d769f6d561671dc038950e3f57543a
Leonardo <leonardo@lrsousa.com.br>
2015-08-24 16:28:20 -0300

    começando a fazer funcao recursiva

--- src/br/com/leitor/Main.java
+++ src/br/com/leitor/Main.java
@@ -6,9 +6,17 @@ public class Main {
 	public static void main(String[] args) {
 		File dir = new File("C:" + File.separator + "Users" + File.separator + "leonardo" + File.separator + "Desktop" + File.separator + "teste");
 		
-		for (File f : dir.listFiles()) {
-			System.out.println(f.getAbsolutePath() + " - " + (f.isDirectory() ? "pasta" : "arquivo"));
-		}
+		leitor(dir.listFiles());
 		
+//		for (File f : dir.listFiles()) {
+//			System.out.println(f.getAbsolutePath() + " - " + (f.isDirectory() ? "pasta" : "arquivo"));
+//		}
+		
+	}
+	
+	public static void leitor(File[] fs) {
+		for (File file : fs) {
+			
+		}
 	}
 }
