397fcf4d7af731a380957fc829b000bc89877242
Leonardo <leonardo@lrsousa.com.br>
2015-08-24 12:25:34 -0300

    add class
    create classes { Main, Pasta}

---
+++ src/br/com/leitor/Main.java
@@ -0,0 +1,7 @@
+package br.com.leitor;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("TESTEE");
+	}
+}
---
+++ src/br/com/leitor/Pasta.java
@@ -0,0 +1,12 @@
+package br.com.leitor;
+
+public class Pasta {
+	private String caminho;
+
+	public String getCaminho() {
+		return caminho;
+	}
+	public void setCaminho(String caminho) {
+		this.caminho = caminho;
+	}
+}
