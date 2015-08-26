edb7c090013c2389fccf6fca778f4106d1520afe
Leonardo <leonardo@lrsousa.com.br>
2015-08-26 14:10:10 -0300

    Versao final
    método recursivo que receber uma lista de "files" e checa se cada um
    deles é arquivo ou pasta e vai navegando pelas pastas e montando umas
    estrutura de ul's e li's em um arquivo html.

--- src/br/com/leitor/Main.java
+++ src/br/com/leitor/Main.java
@@ -1,22 +1,44 @@
 package br.com.leitor;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Main {
-	public static void main(String[] args) {
-		File dir = new File("C:" + File.separator + "Users" + File.separator + "leonardo" + File.separator + "Desktop" + File.separator + "teste");
+	static String desktop = "C:" + File.separator + "Users" + File.separator + "leonardo" + File.separator + "Desktop" + File.separator;
+	static FileWriter html;
+	static PrintWriter escrever;
+	
+	public static void main(String[] args) throws IOException {
 		
+		File dir = new File(desktop + "teste");
+		html = new FileWriter(desktop + "listaArquivos.html");
+		escrever = new PrintWriter(html);
+		
+		System.out.println("Iniciando escrever arquivo...");
+
+		escrever.println("<ul id='" + dir.getAbsolutePath() + "'>");
 		leitor(dir.listFiles());
+		escrever.println("</ul>");
 		
-//		for (File f : dir.listFiles()) {
-//			System.out.println(f.getAbsolutePath() + " - " + (f.isDirectory() ? "pasta" : "arquivo"));
-//		}
+		escrever.close();
+		System.out.println("Fim de escrita");
 		
 	}
 	
 	public static void leitor(File[] fs) {
 		for (File file : fs) {
-			
+			if(file.isFile()) {
+				escrever.println("<li>" + file.getAbsolutePath() + "</li>");
+			} else if(file.isDirectory()) {
+				escrever.println("<li>" + file.getAbsolutePath() + "</li>");
+				if(file.listFiles().length > 0) {
+					escrever.println("<ul id='" + file.getAbsolutePath() + "'>");
+					leitor(file.listFiles());
+					escrever.println("</ul>");
+				}
+			}
 		}
 	}
 }
